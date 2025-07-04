d564403c92f905376889b439c315093ab3087418
Czkafek <czkafek@gmail.com>
2025-07-04 16:19:08 +0200

    login and register buttons in navbar - it still needs small adjustments

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -78,7 +78,7 @@ export default function Navbar() {
 
     return <div className='NavbarContainer'>
         <div className='Navbar'>
-            <Link className={location.pathname == "/" ? "currentPage" : ""} to="/">Strona główna</Link>
+            <Link className={`${location.pathname == "/" ? "currentPage" : ""} option`} to="/">Strona główna</Link>
             <div className='recipesContainer'>
                 <div className='recipes' onMouseEnter={() => setIsHovered(true)}>
                     Przepisy
@@ -116,9 +116,10 @@ export default function Navbar() {
                 </div>
                 }
             </div>
-            <Link className={location.pathname == "/search" ? "currentPage" : ""} to="/search">Szukaj</Link>
-            <Link className={location.pathname == "/account" ? "currentPage" : ""} to="/account">Konto</Link>
-           
+            <Link className={`${location.pathname == "/search" ? "currentPage" : ""} option`} to="/search">Szukaj</Link>
+            <Link className={`${location.pathname == "/account" ? "currentPage" : ""} option`} to="/account">Konto</Link>
+            <Link to="/login" className='loginBtn'><button>Zaloguj się</button></Link>
+            <Link to="/register" className='registerBtn'><button>Zarejestruj się</button></Link>
         </div>
     </div>
 }
