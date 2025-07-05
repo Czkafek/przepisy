f8f20fcba80489c655e23455dcb50260f456a27e
Czkafek <czkafek@gmail.com>
2025-07-05 15:39:14 +0200

    login and register form frontend without functionality is done

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,7 +1,21 @@
 import './Login.styles.css'
 
 export default function Login() {
+
+    const handleSubmit = (e : React.SyntheticEvent) => {
+        e.preventDefault();
+        console.log(e);
+    }
+
+
     return <>
-        <h1>Login page</h1>
+        <form className='accountForm' onSubmit={handleSubmit}>
+            <h2>Zaloguj się</h2>
+            <label>Nazwa lub Email</label>
+            <input type="text" placeholder='Nazwa lub email' />
+            <label>Hasło</label>
+            <input type="password" placeholder='Hasło' />
+            <input type="submit" value='Zaloguj się' />
+        </form>
     </>
 }
--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,7 +1,23 @@
 import './Register.styles.css'
 
 export default function Register() {
+
+    const handleSubmit = (e : React.SyntheticEvent) => {
+        e.preventDefault();
+        console.log(e);
+    }
+
+
     return <>
-        <h1>Register page</h1>
+        <form className='accountForm' onSubmit={handleSubmit}>
+            <h2>Zarejestruj się</h2>
+            <label>Nazwa</label>
+            <input type="text" placeholder='Nazwa' />
+            <label>Email</label>
+            <input type="text" placeholder='Email' />
+            <label>Hasło</label>
+            <input type="password" placeholder='Hasło' />
+            <input type="submit" value='Zaloguj się' />
+        </form>
     </>
 }
